2aaf24d1089f3c18f643551f286da1fec51374e6
AYork <yorkdam@gmail.com>
2016-10-04 15:18:01 +0200

    Init v2

---
+++ webFormExample/webFormExample/index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="webFormExample.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="grdUser" runat="server" AutoGenerateColumns="False">
+        </asp:GridView>
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ webFormExample/webFormExample/index.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace webFormExample
+{
+    public partial class index : System.Web.UI.Page
+    {
+        DataTable dT;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dataLoad();
+
+        }
+
+        private void dataLoad()
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings[""].ConnectionString;
+                string SQL = "SELECT * FROM  users";
+                SqlConnection conn = new SqlConnection(connectionString);
+                conn.Open();
+                DataSet dS = new DataSet();
+                SqlDataAdapter dAdapter = new SqlDataAdapter(SQL, conn);
+                dAdapter.Fill(dS);
+                //grdUser
+                dT = dS.Tables[0];
+                grdUser.DataSource = dT;
+                grdUser.DataBind();
+                conn.Close();
+                
+            }
+            catch (SqlException ex)
+            {
+
+                Console.Error.Write(ex.Message);
+            }
+           
+
+
+        }
+    }
+}
